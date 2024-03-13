3b245ecca5f41cd3a74869cc2b12c487f0deda41
fasiash <fasiuddin@codetru.com>
2024-03-13 13:55:56 +0530

    removed chrome

--- src/test/java/TestCases/TestCases_of_CICA.java
+++ src/test/java/TestCases/TestCases_of_CICA.java
@@ -115,7 +115,7 @@ public class TestCases_of_CICA{
 	@BeforeTest
 	public void chromeLaunch() {
 //		WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "C://Users//Codetru//eclipse-workspace//SampleCodetruProject//Citizens_ProjectNewUpdated//BrowserDriver//chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C://Users//Codetru//eclipse-workspace//SampleCodetruProject//Citizens_ProjectNewUpdated//BrowserDriver//chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--allow-running-insecure-content");
 		options.addArguments("--remote-allow-origins=*");
