869aae419f604567416d714adb056de9267d9879
fasiash <fasiuddin@codetru.com>
2024-04-25 15:51:45 +0530

    added chromedriver in driver folder

--- src/test/java/TestCases/TestCases_of_CICA.java
+++ src/test/java/TestCases/TestCases_of_CICA.java
@@ -124,8 +124,8 @@ public class TestCases_of_CICA{
 	
 	@BeforeTest
 	public void chromeLaunch() {
-		WebDriverManager.chromedriver().setup();
-//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Codetru\\Desktop\\chromedriver.exe");
+//		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C://Users//Codetru//eclipse-workspace//SampleCodetruProject//Citizens_ProjectNewUpdated2//drivers//chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--allow-running-insecure-content");
 		options.addArguments("--remote-allow-origins=*");
