5401c20b8f46df03f18797f7892da3036e382588
fasiash <fasiuddin@codetru.com>
2024-05-09 10:28:59 +0530

    added chromedriver

--- src/test/java/TestCases/TestCases_of_CICA.java
+++ src/test/java/TestCases/TestCases_of_CICA.java
@@ -126,11 +126,10 @@ public class TestCases_of_CICA{
 	@BeforeTest
 	public void chromeLaunch() {
 //		WebDriverManager.chromedriver().setup();
-		System.setProperty("webdriver.chrome.driver", "C://Users//Codetru//eclipse-workspace//SampleCodetruProject//Citizens_ProjectNewUpdated2//drivers//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Codetru\\eclipse-workspace\\SampleCodetruProject\\Citizens_ProjectNewUpdated2\\drivers\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--allow-running-insecure-content");
-		options.addArguments("--remote-allow-origins=*");
-		
+		options.addArguments("--remote-allow-origins=*");	
 		driver = new ChromeDriver(options);
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();	
